e95b88b4586881ef1341ad00cb31993a804599c9
3DUCK <kermit7520k@gmail.com>
2024-07-03 01:42:40 +0900

    C:/Program Files/Git/home/plus

--- src/main/java/com/example/basic/HomeController.java
+++ src/main/java/com/example/basic/HomeController.java
@@ -2,6 +2,7 @@ package com.example.basic;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 // @Controller 의미
@@ -20,7 +21,6 @@ public class HomeController {
     // @GetMapping("/home/main") 의미
     // 개발자가 스프링부트에게 말한다
     // 만약에 /home/main 이런 요청이 들어 오면 아래 메서드를 실행해줘
-
     @GetMapping("/home/main")
 
     // @ResponseBody 의미
@@ -47,6 +47,18 @@ public class HomeController {
     public int showIncrease() {
         count++;
         return count;
+    }
+
+    @GetMapping("/home/plus")
+    @ResponseBody
+    // RequestParam의 의미
+    // 개발자가 스프링부트에게 말한다.
+    // int a 는 쿼리스트링에서 a파라미터의 값을 얻은 후 정수화 한 값이어야 한다.
+    // @RequestParam은 생략 가능
+    public int showPlus(@RequestParam(value = "a", defaultValue = "0") int a,
+                        @RequestParam(value = "a", defaultValue = "0") int b)
+    {
+        return a + b;
     }
 
 }
